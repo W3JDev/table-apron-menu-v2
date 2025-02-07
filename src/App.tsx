888d865d1e59341c5f6f41b70d18fa0b86372bb7
MN Jewel <w3j.btc@gmail.com>
2025-02-08 01:03:01 +0800

    Add main App component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MenuApp from './components/MenuApp';
+
+function App() {
+  return (
+    <div className="App">
+      <MenuApp />
+    </div>
+  );
+}
+
+export default App;
